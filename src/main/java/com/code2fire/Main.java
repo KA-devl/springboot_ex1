9649a087ec38ecdbce45fd0c454da3d4ceaa1647
Anes Khadiri <anes1999@live.ca>
2023-05-09 19:50:15 +0200

    Initial commit

---
+++ src/main/java/com/code2fire/Main.java
@@ -0,0 +1,21 @@
+package com.code2fire;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Running application!");
+        SpringApplication.run(Main.class, args);
+    }
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World!";
+    }
+}
