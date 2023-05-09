0fea55228930661acf30803e3cec86622632b8c9
Anes Khadiri <anes1999@live.ca>
2023-05-09 22:14:27 +0200

    Fixed docker bugs

--- src/main/java/com/code2fire/Main.java
+++ src/main/java/com/code2fire/Main.java
@@ -3,11 +3,12 @@ package com.code2fire;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @RestController
 public class Main {
     public static void main(String[] args) {
