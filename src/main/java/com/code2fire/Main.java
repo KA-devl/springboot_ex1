0cf733c30499d476ebe64b8f56e80d6e697479c4
Anes Khadiri <anes1999@live.ca>
2023-05-10 14:53:42 +0200

    Add get customers endpoint

--- src/main/java/com/code2fire/Main.java
+++ src/main/java/com/code2fire/Main.java
@@ -1,24 +1,28 @@
 package com.code2fire;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
+
 
 @SpringBootApplication
 @RestController
 public class Main {
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args) {
         System.out.println("Running application!");
         SpringApplication.run(Main.class, args);
     }
-    @GetMapping("/")
-    public Greetings hello() {
-        return new Greetings("Hello World!");
+    @GetMapping("/customers")
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
     }
 
-    record Greetings(String message) {}
 }
