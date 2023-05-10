467a28d8ffc31614d144dc2a0bcc74849509fdd0
Anes Khadiri <anes1999@live.ca>
2023-05-10 15:32:37 +0200

    Added POST customers endpoint

--- src/main/java/com/code2fire/Main.java
+++ src/main/java/com/code2fire/Main.java
@@ -2,8 +2,8 @@ package com.code2fire;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.*;
 
 
@@ -25,4 +25,14 @@ public class Main {
         return customerRepository.findAll();
     }
 
+    record NewCustomerRequest(String name, String email, String age) {}
+    @PostMapping("/customers")
+    public void addCustomer(@RequestBody NewCustomerRequest request) {
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
+
+    }
 }
